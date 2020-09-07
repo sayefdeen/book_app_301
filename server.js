ec4f740ef93c6409169777f38d9bfb16b18502aa
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-09-07 17:49:12 +0300

    adding handle error fun

--- server.js
+++ server.js
@@ -41,9 +41,18 @@ function renderNewSearch(req, res) {
 
 function renderShowBooksPage(req, res) {
   const selectSQL = 'SELECT * FROM books_table';
-  client.query(selectSQL).then((result) => {
-    res.render('pages/books/show', { books: result.rows });
-  });
+  client
+    .query(selectSQL)
+    .then((result) => {
+      res.render('pages/books/show', { books: result.rows });
+    })
+    .catch(() => {
+      errorPage(
+        req,
+        res,
+        'There is an error in rendering the data from database'
+      );
+    });
 }
 
 // Get the Book data From the API
@@ -52,12 +61,17 @@ function postSearchHanlde(req, res) {
   const titleORauthor = data.select;
   const name = data.searchbox;
   const url = `https://www.googleapis.com/books/v1/volumes?q=+in${titleORauthor}:${name}`;
-  superAgent.get(url).then((results) => {
-    let bookArray = results.body.items.map((book) => {
-      return new Books(book);
+  superAgent
+    .get(url)
+    .then((results) => {
+      let bookArray = results.body.items.map((book) => {
+        return new Books(book);
+      });
+      res.render('pages/searches/show', { books: bookArray });
+    })
+    .catch(() => {
+      errorPage(req, res, 'There is an error in the search API');
     });
-    res.render('pages/searches/show', { books: bookArray });
-  });
 }
 
 function insertBookIntoDB(req, res) {
@@ -66,18 +80,28 @@ function insertBookIntoDB(req, res) {
   let insertSQL =
     'INSERT INTO books_table (author,title,isbn,image_url,descriptions) VALUES ($1,$2,$3,$4,$5)';
   let safeValues = [author, title, isbn, image_url, descriptions];
-  client.query(insertSQL, safeValues).then(() => {
-    console.log('added to dataBase');
-  });
+  client
+    .query(insertSQL, safeValues)
+    .then(() => {
+      console.log('added to dataBase');
+    })
+    .catch(() => {
+      errorPage(req, res, 'There is an error in the insert query');
+    });
 }
 
 function viewBookDetails(req, res) {
   let selectSQL = 'SELECT * FROM books_table WHERE id =$1';
   const id = req.params.bookID;
   let values = [id];
-  client.query(selectSQL, values).then((data) => {
-    res.render('pages/books/detail', { books: data.rows });
-  });
+  client
+    .query(selectSQL, values)
+    .then((data) => {
+      res.render('pages/books/detail', { books: data.rows });
+    })
+    .catch(() => {
+      errorPage(req, res, 'There is an error in viewing details');
+    });
 }
 
 // ******************* Constructors ***************
@@ -87,11 +111,19 @@ function viewBookDetails(req, res) {
 function Books(book) {
   this.author = book.volumeInfo.authors || `There is no authors`;
   this.title = book.volumeInfo.title;
-  this.isbn =
-    book.volumeInfo.industryIdentifiers[0].identifier || 'There is no isbn';
+  // this.isbn =
+  //   book.volumeInfo.industryIdentifiers[0].identifier || 'There is no isbn';
+  this.isbn = book.volumeInfo.industryIdentifiers
+    ? book.volumeInfo.industryIdentifiers[0].identifier || 'There is no isbn'
+    : 'There is no isbn';
   this.image_url =
     book.volumeInfo.imageLinks.thumbnail || `https://i.imgur.com/J5LVHEL.jpg`;
   this.descriptions = book.volumeInfo.description || `There is no description`;
+}
+
+// Error Function
+function errorPage(req, res, massage = `Sorry,something went wrong`) {
+  res.render('/pages/error', { error: massage });
 }
 
 // Make Sure the Server is working
