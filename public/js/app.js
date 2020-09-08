4552e8dbe701a2f70c64bcf263d3b16e658ea590
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-09-08 22:41:38 +0300

    finish the nav bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,13 @@
+// For the Update button in details
+
 $('#updateForm').hide();
 
 $('#updateBtn').click(() => {
   $('#updateForm').toggle();
+});
+
+// For the Drop Button
+
+$('#dropbtn').click(() => {
+  $('#nav-content a').slideToggle('slow');
 });
